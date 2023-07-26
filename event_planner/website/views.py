73c10e77146d0aa9bce6fab249621c75c12726eb
Okwudili Ezeme <mellitus4u@gmail.com>
2023-07-26 01:35:06 -0400

    Added the welcome page view

--- event_planner/website/views.py
+++ event_planner/website/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def welcome(request):
+    """
+        A function to render the welcome page.
+    Args:
+        request (request): the request object
+
+    Returns:
+        HttpResponse: Message to the user
+    """
+    return HttpResponse("Welcome to the Event Planner!")
