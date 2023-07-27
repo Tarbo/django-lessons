3f8e932c73033d1d2e2f722cbbd0998107aabdda
Okwudili Ezeme <mellitus4u@gmail.com>
2023-07-27 03:02:30 -0400

    Added the room table to the data model of meetings

--- event_planner/meetings/models.py
+++ event_planner/meetings/models.py
@@ -20,3 +20,22 @@ class Meeting(models.Model):
         db_table = 'meeting'
         verbose_name_plural = 'meeting'
         ordering = ['meeting_date']
+
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=255)
+    room_capacity = models.IntegerField(default=2)
+    floor_number = models.IntegerField(default=0)
+    room_number = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.room_name} at {self.floor_number} floor with a room number {self.room_number} has capacity of ' \
+               f'{self.room_capacity}'
+
+    class Meta:
+        """
+            A class to represent the meta data of the Rooms model.
+        """
+        db_table = 'room'
+        verbose_name_plural = 'room'
+        ordering = ['room_name']
